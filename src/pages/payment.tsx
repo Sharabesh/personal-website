1544025f4c321a3133994b5654d7e209a599603c
sharabesh <sharabesh@makenotion.com>
2023-12-30 13:50:09 -0500

    fixup

---
+++ src/pages/payment.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+const Payment = () => (
+  <div className="min-h-screen flex items-center justify-center bg-gray-200 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="max-w-md w-full space-y-8 text-center bg-white p-10 rounded-lg shadow-md">
+      <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        Bilt Card Subscription
+      </h2>
+      <p className="mt-2 text-center text-gray-600">
+        Make easy subscriptions using this payment link
+      </p>
+
+      <div className="mt-5">
+        <Image
+          src={"/payment_link.png"}
+          alt={"pay with stripe"}
+          width={300}
+          height={300}
+          className="object-cover rounded-md"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+export default Payment;
